7d1d8026de230adce9b1e34780f27b1c2039ad7d
aksprat <apratiush@digitalocean.com>
2025-07-29 17:07:41 +0530

    Configure Epic Stack for DigitalOcean deployment

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -1,37 +1,62 @@
-import { styleText } from 'node:util'
-import { remember } from '@epic-web/remember'
-// Changed import due to issue: https://github.com/remix-run/react-router/pull/12644
-import { PrismaClient } from '@prisma/client/index.js'
+import { PrismaClient } from '@prisma/client'
 
-export const prisma = remember('prisma', () => {
-	// NOTE: if you change anything in this function you'll need to restart
-	// the dev server to see your changes.
+let prisma: PrismaClient
 
-	// Feel free to change this log threshold to something that makes sense for you
-	const logThreshold = 20
+declare global {
+  var __db__: PrismaClient
+}
 
-	const client = new PrismaClient({
-		log: [
-			{ level: 'query', emit: 'event' },
-			{ level: 'error', emit: 'stdout' },
-			{ level: 'warn', emit: 'stdout' },
-		],
-	})
-	client.$on('query', async (e) => {
-		if (e.duration < logThreshold) return
-		const color =
-			e.duration < logThreshold * 1.1
-				? 'green'
-				: e.duration < logThreshold * 1.2
-					? 'blue'
-					: e.duration < logThreshold * 1.3
-						? 'yellow'
-						: e.duration < logThreshold * 1.4
-							? 'redBright'
-							: 'red'
-		const dur = styleText(color, `${e.duration}ms`)
-		console.info(`prisma:query - ${dur} - ${e.query}`)
-	})
-	void client.$connect()
-	return client
-})
+// This is needed because in development we don't want to restart
+// the server with every change, but we want to make sure we don't
+// create a new connection to the DB with every change either.
+// In production, we'll have a single connection to the DB.
+if (process.env.NODE_ENV === 'production') {
+  prisma = getClient()
+} else {
+  if (!global.__db__) {
+    global.__db__ = getClient()
+  }
+  prisma = global.__db__
+}
+
+function getClient() {
+  const { DATABASE_URL } = process.env
+  if (!DATABASE_URL) {
+    throw new Error('DATABASE_URL environment variable is required')
+  }
+  
+  const databaseUrl = new URL(DATABASE_URL)
+
+  const isLocalSQLite = databaseUrl.protocol === 'file:'
+
+  const PRIMARY_REGION = process.env.PRIMARY_REGION
+  const FLY_REGION = process.env.FLY_REGION
+
+  const isReadReplicaRegion = !isLocalSQLite && PRIMARY_REGION && FLY_REGION && PRIMARY_REGION !== FLY_REGION
+
+  if (!isLocalSQLite && isReadReplicaRegion) {
+    // 5433 is the read-replica port
+    databaseUrl.port = '5433'
+    // ensure readonly
+    if (!databaseUrl.searchParams.has('sslmode')) {
+      databaseUrl.searchParams.set('sslmode', 'require')
+    }
+  } else if (!isLocalSQLite) {
+    // make sure we have a connection limit
+    databaseUrl.searchParams.set('connection_limit', '1')
+  }
+
+  const client = new PrismaClient({
+    datasources: {
+      db: {
+        url: databaseUrl.toString(),
+      },
+    },
+  })
+  // connect eagerly
+  client.$connect()
+
+  return client
+}
+
+export { prisma }
